2bc81795da964c529ae75597038f60d9b986a3fd
Patryk Lasko <lachupro@gmail.com>
2023-12-21 03:17:17 +0100

    Update FileController.cs

--- VisualSoft/VisualSoft/Controllers/FileController.cs
+++ VisualSoft/VisualSoft/Controllers/FileController.cs
@@ -1,11 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Globalization;
-using System.Reflection.Metadata;
-using VisualSoft.Models;
 using VisualSoft.Models.Api;
 using VisualSoft.Services;
-using Document = VisualSoft.Models.Document;
-
+    
 namespace VisualSoft.Controllers
 {
     [ApiController]
